8ba15f58b3dc54718835a389f993ccd162db6faf
inactivist <inactivist@gmail.com>
2012-11-17 10:56:25 -0800

    Adding location-query macros and unit tests.
    We now support --location-query macros: 'any', 'all', 'global' == all geotagged tweets.  'usa' and 'continental-usa' == all within continental USA.
    
    tests.py: Initial unit test implementation
     - Adding test_location_macros() to test new location macro facility.

--- streamer/streamer.py
+++ streamer/streamer.py
@@ -13,6 +13,34 @@ logger = logging.getLogger(__name__)
 RETRY_LIMIT = 10
 MISSING_FIELD_VALUE = ''
 
+"""
+    List of named location query (--location-query) names and their
+    associated bounding boxes (longitude, latitude) (what is the name of that standard?  geoJSON?)
+
+    Recursive lookups:
+    If a value is string, it's a reference to a named entry in the table.  Use the remainder of the
+    value string as the new lookup key.
+"""
+LOCATION_QUERY_MACROS = {
+    'any': [-180,-90,180,90],
+    'all': 'any',
+    'global': 'any',
+    'usa': [-124.848974,24.396308,-66.885444,49.384358], # http://www.openstreetmap.org/?box=yes&bbox=-124.848974,24.396308,-66.885444,49.384358
+    'contintental_usa': 'usa'
+}
+
+
+def lookup_location_query_macro(name):
+    """
+    Look up location query name in macro table.
+    Return list of coordinates of bounding box as floating point numbers, or None if
+    not found.
+    """
+    resolved = LOCATION_QUERY_MACROS.get(name.lower())
+    if isinstance(resolved, basestring):
+        return lookup_location_query_macro(resolved)
+    return resolved
+
 
 def csv_args(value):
     """Parse a CSV string into a Python list of strings.
@@ -42,6 +70,7 @@ def _init_logger(config, opts):
 
 
 class StreamListener(tweepy.StreamListener):
+    
     def __init__(self, opts, api=None):
         super(StreamListener, self).__init__(api=api)
         self.opts = opts
@@ -117,6 +146,7 @@ class StreamListener(tweepy.StreamListener):
                     logger.warn(f, exc_info=e)
                     pass
             else:
+                # Raw JSON stream data output:
                 print stream_data.strip()
 
         # Parse stream_data, compare tweet timestamp to current time as GMT;
@@ -188,6 +218,9 @@ class StreamListener(tweepy.StreamListener):
         return
 
 def location_query_to_location_filter(tweepy_auth, location_query):
+    t = lookup_location_query_macro(location_query)
+    if t:
+        return t
     api = tweepy.API(tweepy_auth)
     # Normalize whitespace to single spaces.
     places = api.geo_search(query=location_query)
@@ -204,6 +237,8 @@ def location_query_to_location_filter(tweepy_auth, location_query):
                 return t
             else:
                 raise ValueError("Place '%s' does not have a bounding box." % place.full_name)
+            
+    # Nothing found, try for matching macro
     raise ValueError("'%s': No such place." % location_query)
 
 
--- streamer/tests.py
+++ streamer/tests.py
@@ -1,2 +1,19 @@
 import unittest
 
+class Tests(unittest.TestCase):
+    def test_location_macros(self):
+        import streamer
+        r = streamer.lookup_location_query_macro('usa')
+        self.assertIsNotNone(r)
+        usa_bbox = [-124.848974,24.396308,-66.885444,49.384358]
+        for i, x in enumerate(r):
+            self.assertAlmostEqual(x, usa_bbox[i], 5)
+        r = streamer.lookup_location_query_macro('bogus')
+        self.assertIsNone(r)
+        r = streamer.lookup_location_query_macro('contintental_usa')
+        self.assertIsNotNone(r)
+        self.assertIsInstance(r, list)
+        self.assertListEqual(r, streamer.lookup_location_query_macro('usa'))
+                    
+if __name__ == '__main__':
+    unittest.main()
