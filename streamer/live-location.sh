c1758d207668b71f34ed9975c121d17568f5bd6a
inactivist <inactivist@gmail.com>
2012-11-21 08:18:03 -0800

    Moving live-location.sh to another project, since it doesn't belong here.

--- streamer/live-location.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-FEEDMAPPER=~/workspace/feedmapper/mapper
-PUMP=$FEEDMAPPER/json-pump.py
-STREAMER_OPTS="-l DEBUG"
-python streamer.py $STREAMER_OPTS -c dev.ini --location-query="$1" | python $PUMP
