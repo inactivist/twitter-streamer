0c536c9f25f0f0f115ea5c1a3c15a084df82f51d
Michael Curry <mike@exodusdev.com>
2020-09-09 21:22:14 -0700

    Reformat using black, simplify options validation cruft

--- streamer/streamer.py
+++ streamer/streamer.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
+import logging
 import os
 import sys
-import logging
 import time
-import datetime
+
 import tweepy
-import simplejson as json
-import utils
 
 import args
+import utils
 from listener import StreamListener
+
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 
@@ -25,11 +25,16 @@ RETRY_LIMIT = 10
     value string as the new lookup key.
 """
 LOCATION_QUERY_MACROS = {
-    'any': [-180,-90,180,90],
-    'all': 'any',
-    'global': 'any',
-    'usa': [-124.848974,24.396308,-66.885444,49.384358], # http://www.openstreetmap.org/?box=yes&bbox=-124.848974,24.396308,-66.885444,49.384358
-    'contintental_usa': 'usa'
+    "any": [-180, -90, 180, 90],
+    "all": "any",
+    "global": "any",
+    "usa": [
+        -124.848974,
+        24.396308,
+        -66.885444,
+        49.384358,
+    ],  # http://www.openstreetmap.org/?box=yes&bbox=-124.848974,24.396308,-66.885444,49.384358
+    "contintental_usa": "usa",
 }
 
 
@@ -45,9 +50,9 @@ def lookup_location_query_macro(name):
     return resolved
 
 
-
 def get_version():
     from __init__ import __version__
+
     return __version__
 
 
@@ -58,19 +63,27 @@ def location_query_to_location_filter(tweepy_auth, location_query):
     api = tweepy.API(tweepy_auth)
     # Normalize whitespace to single spaces.
     places = api.geo_search(query=location_query)
-    normalized_location_query = location_query.replace(' ', '')
+    normalized_location_query = location_query.replace(" ", "")
     for place in places:
         logger.debug('Considering place "%s"' % place.full_name)
         # Normalize spaces
-        if place.full_name.replace(' ', '').lower() == normalized_location_query.lower():
-            logger.info('Found matching place: full_name=%(full_name)s id=%(id)s url=%(url)s' % place.__dict__)
+        if (
+            place.full_name.replace(" ", "").lower()
+            == normalized_location_query.lower()
+        ):
+            logger.info(
+                "Found matching place: full_name=%(full_name)s id=%(id)s url=%(url)s"
+                % place.__dict__
+            )
             if place.bounding_box is not None:
                 t = [x for x in place.bounding_box.origin()]
                 t.extend([x for x in place.bounding_box.corner()])
-                logger.info('  location box: %s' % t)
+                logger.info("  location box: %s" % t)
                 return t
             else:
-                raise ValueError("Place '%s' does not have a bounding box." % place.full_name)
+                raise ValueError(
+                    "Place '%s' does not have a bounding box." % place.full_name
+                )
 
     # Nothing found, try for matching macro
     raise ValueError("'%s': No such place." % location_query)
@@ -79,38 +92,47 @@ def location_query_to_location_filter(tweepy_auth, location_query):
 def make_filter_args(opts, tweepy_auth):
     kwargs = {}
     if opts.track:
-        kwargs['track'] = opts.track
+        kwargs["track"] = opts.track
     if opts.stall_warnings:
-        kwargs['stall_warnings'] = True
+        kwargs["stall_warnings"] = True
     if opts.locations:
-        kwargs['locations'] = map(float, opts.locations)
+        kwargs["locations"] = map(float, opts.locations)
     if opts.location_query:
-        kwargs['locations'] = location_query_to_location_filter(tweepy_auth, opts.location_query)
+        kwargs["locations"] = location_query_to_location_filter(
+            tweepy_auth, opts.location_query
+        )
     if opts.follow:
-        kwargs['follow'] = opts.follow
+        kwargs["follow"] = opts.follow
     return kwargs
 
 
 def process_tweets(opts):
     """Set up and process incoming streams."""
     try:
-        consumer_key = os.environ['CONSUMER_KEY']
-        consumer_secret = os.environ['CONSUMER_SECRET']
-        access_key = os.environ['ACCESS_KEY']
-        access_secret = os.environ['ACCESS_SECRET']
-        logger.debug('consumer_key={consumer_key}, consumer_secret={consumer_secret}'.format(**locals()))
-        logger.debug('access_key={access_key}, access_secret={access_secret}'.format(**locals()))
+        consumer_key = os.environ["CONSUMER_KEY"]
+        consumer_secret = os.environ["CONSUMER_SECRET"]
+        access_key = os.environ["ACCESS_KEY"]
+        access_secret = os.environ["ACCESS_SECRET"]
+        logger.debug(
+            "consumer_key={consumer_key}, consumer_secret={consumer_secret}".format(
+                **locals()
+            )
+        )
+        logger.debug(
+            "access_key={access_key}, access_secret={access_secret}".format(**locals())
+        )
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_key, access_secret)
     except KeyError as e:
-        logger.error('You must set the %s API key environment variable.', e)
+        logger.error("You must set the %s API key environment variable.", e)
         return
 
-    logger.debug('Init tweepy.Stream()')
+    logger.debug("Init tweepy.Stream()")
     logger.debug(opts)
     listener = StreamListener(opts=opts, logger=logger)
-    streamer = tweepy.Stream(auth=auth, listener=listener, retry_count=9999,
-        retry_time=1, buffer_size=16000)
+    streamer = tweepy.Stream(
+        auth=auth, listener=listener, retry_count=9999, retry_time=1, buffer_size=16000
+    )
 
     try:
         kwargs = make_filter_args(opts, auth)
@@ -122,21 +144,23 @@ def process_tweets(opts):
     while listener.running:
         try:
             try:
-                logger.debug('streamer.filter(%s)', kwargs)
+                logger.debug("streamer.filter(%s)", kwargs)
                 streamer.filter(**kwargs)
             except TypeError as e:
-                if 'stall_warnings' in e.message:
-                    logger.warn("Installed Tweepy version doesn't support stall_warnings parameter.  Restarting without stall_warnings parameter.")
-                    del kwargs['stall_warnings']
+                if "stall_warnings" in e.message:
+                    logger.warn(
+                        "Installed Tweepy version doesn't support stall_warnings parameter.  Restarting without stall_warnings parameter."
+                    )
+                    del kwargs["stall_warnings"]
                     streamer.filter(**kwargs)
                 else:
                     raise
 
-            logger.debug('Returned from streaming...')
+            logger.debug("Returned from streaming...")
         except IOError:
             if opts.terminate_on_error:
                 listener.running = False
-            logger.exception('Caught IOError')
+            logger.exception("Caught IOError")
         except KeyboardInterrupt:
             logger.warning("KeyboardInterrupt")
             # Stop the listener loop.
@@ -146,22 +170,21 @@ def process_tweets(opts):
             logger.exception("Unexpected exception.")
 
         if listener.running:
-            logger.debug('Sleeping...')
+            logger.debug("Sleeping...")
             time.sleep(5)
 
 
-
 if __name__ == "__main__":
-    import config
     opts = args.parse_command_line(get_version())
 
     # TODO: Fix this -
-    if (opts.location_query is None and
-        opts.locations is None and
-        opts.follow is None):
+    if not (opts.location_query or opts.locations or opts.follow):
         if not opts.track:
-            sys.stderr.write('%s: error: Must provide list of track keywords if --location or --location-query is not provided.\n' % __file__)
+            sys.stderr.write(
+                "%s: error: Must provide list of track keywords if --location, --location-query, or --follow arguments are not provided.\n"
+                % __file__
+            )
             sys.exit()
     utils.init_logger(logger, opts.log_level)
-    logger.debug('opts=%s', opts.__dict__)
+    logger.debug("opts=%s", opts.__dict__)
     process_tweets(opts)
