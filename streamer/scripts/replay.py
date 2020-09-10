3e2a3f3543724637c646c9bc7e3f9be9d160ab7a
Michael Curry <mike@exodusdev.com>
2020-09-09 21:53:58 -0700

    Commit old changes to replay.py; needs testing

--- streamer/scripts/replay.py
+++ streamer/scripts/replay.py
@@ -7,6 +7,7 @@ How to do continuous play if coming from stdin?
 Or is that only possible if coming from a named file?
  - http://mail.python.org/pipermail/tutor/2003-May/022520.html
 """
+import os
 import sys
 import time
 import datetime
@@ -15,7 +16,6 @@ logger = logging.getLogger(__name__)
 import simplejson as json
 
 TWITTER_TIME_FORMAT = '%a %b %d %H:%M:%S +0000 %Y'
-MAX_SLEEP_TIME = 1
 
 def parse_twitter_time(time_str):
     return datetime.datetime.strptime(time_str, TWITTER_TIME_FORMAT)
@@ -36,9 +36,11 @@ def _init_logger(level):
     logger.setLevel(level)
 
 
-_init_logger("WARNING")
+_init_logger(os.environ.get('LOGLEVEL', 'WARN'))
 last_time = None
 start_time = datetime.datetime.utcnow()
+replay_mult = 10.0 # replay at x * normal speed
+sleep_factor = 1.0 / replay_mult
 
 for line in sys.stdin:
     if not line.strip():
@@ -64,17 +66,15 @@ for line in sys.stdin:
         current = parse_twitter_time(status.get('created_at', last_time))
         delta = current - last_time
         sleep_time = delta.total_seconds()
-        if sleep_time < 0 or sleep_time > MAX_SLEEP_TIME:
-            sys.stderr.write(
-                "sleep_time (%d) outside bounds for tweet id %s\n" % (
-                    sleep_time,
-                    status.get('id_str')))
-            sys.stderr.flush()
+        if sleep_time < 0:
+            logger.warn("sleep_time (%f) outside bounds for tweet id %s" % (
+                sleep_time,
+                status.get('id_str')))
+            sleep_time = 0
         # Clamp sleep_time to 0 <= sleep_time <= MAX_SLEEP_TIME
-        sleep_time = max(0, min(sleep_time, MAX_SLEEP_TIME))
+        # sleep_time = max(0, min(sleep_time, MAX_SLEEP_TIME))
         last_time = current
-        time.sleep(sleep_time)
+        time.sleep(sleep_time * sleep_factor)
 
-    logger.debug('replay: Writing line: %s', line[:30])
     sys.stdout.write(line)
     sys.stdout.flush()
