be88c52a68e2456dcacb7f37c847b3530e240a92
Michael Curry <mike@exodusdev.com>
2020-09-24 22:51:27 -0700

    Eliminate simplejson

--- streamer/scripts/replay.py
+++ streamer/scripts/replay.py
@@ -7,18 +7,21 @@ How to do continuous play if coming from stdin?
 Or is that only possible if coming from a named file?
  - http://mail.python.org/pipermail/tutor/2003-May/022520.html
 """
+import datetime
+import json
+import logging
 import os
 import sys
 import time
-import datetime
-import logging
-logger = logging.getLogger(__name__)
-import simplejson as json
 
-TWITTER_TIME_FORMAT = '%a %b %d %H:%M:%S +0000 %Y'
+logger = logging.getLogger(__name__)
+
+TWITTER_TIME_FORMAT = "%a %b %d %H:%M:%S +0000 %Y"
+
 
 def parse_twitter_time(time_str):
     return datetime.datetime.strptime(time_str, TWITTER_TIME_FORMAT)
+
 
 def datetime_to_unixtime(dt):
     return time.mktime(dt.timetuple())
@@ -31,21 +34,22 @@ def datetime_to_twitter_time_string(dt):
 
 def _init_logger(level):
     from logging import _checkLevel
+
     FORMAT = "%(asctime)-15s %(message)s"
     logging.basicConfig(format=FORMAT)
     logger.setLevel(level)
 
 
-_init_logger(os.environ.get('LOGLEVEL', 'WARN'))
+_init_logger(os.environ.get("LOGLEVEL", "WARN"))
 last_time = None
 start_time = datetime.datetime.utcnow()
-replay_mult = 10.0 # replay at x * normal speed
+replay_mult = 10.0  # replay at x * normal speed
 sleep_factor = 1.0 / replay_mult
 
 for line in sys.stdin:
     if not line.strip():
         # Line is empty, continue.
-        logger.debug('Line is empty, continuing...')
+        logger.debug("Line is empty, continuing...")
         continue
     try:
         status = json.loads(line)
@@ -53,23 +57,24 @@ for line in sys.stdin:
         logger.warn("Error parsing line '%s', continuing", line)
         continue
     except Exception as e:
-        logger.exception('Fatal error parsing %s.', line)
+        logger.exception("Fatal error parsing %s.", line)
         sys.exit(1)
 
     # If first item, emit it immediately.
     # Else, calculate time delta from last status and delay the appropriate amount of time,
     # if any is required.
     if not last_time:
-        last_time = parse_twitter_time(status.get('created_at'))
+        last_time = parse_twitter_time(status.get("created_at"))
     else:
         # get time delta in seconds.
-        current = parse_twitter_time(status.get('created_at', last_time))
+        current = parse_twitter_time(status.get("created_at", last_time))
         delta = current - last_time
         sleep_time = delta.total_seconds()
         if sleep_time < 0:
-            logger.warn("sleep_time (%f) outside bounds for tweet id %s" % (
-                sleep_time,
-                status.get('id_str')))
+            logger.warn(
+                "sleep_time (%f) outside bounds for tweet id %s"
+                % (sleep_time, status.get("id_str"))
+            )
             sleep_time = 0
         # Clamp sleep_time to 0 <= sleep_time <= MAX_SLEEP_TIME
         # sleep_time = max(0, min(sleep_time, MAX_SLEEP_TIME))
